e162b9f89d9efb39e4e63d256a2a205fb2968c8f
aNjALiiiii <38291012+aNjALiiiii@users.noreply.github.com>
2018-05-29 21:55:13 -0700

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,17 @@
+print("--PYTHON--")
+a = "Acad"
+b = "view"
+c = a+b
+print(c)
+print("Enter the value of x,y and z")
+x,y,z = input(),input(),input()
+print(x,y,z)
+print("Let's get started")
+#print('Let\'s get started')
+s = "Acadview"
+course = "Python"
+fees = 5000
+print(s,course,fees,sep=',')
+name = "Tony Stark"
+salary = 1000000
+print('%s %d' % (name,salary))
